e45c6bc527c3e71fd823c541da664dbc2e7f73da
Kaielth Craftlay <carlos.marquez.g@outlook.com>
2015-08-11 21:52:04 -0600

    Implemented long queue list, must work for N elements waiting in the queue, tested for 3 elements

--- Assets/Scripts/Queue.cs
+++ Assets/Scripts/Queue.cs
@@ -3,41 +3,69 @@ using System.Collections;
 
 public class Queue : MonoBehaviour {
 
-	//
-	public int q;
+	//Properties
+	public int[] q;
+	public int qLimit;
 	public GameObject[] groups;
 
-	// Use this for initialization
+	// initialization
 	void Start () {
-		newPiece ();
-		addToQueueList ();
+		qLimit = 3;
+		q = new int[qLimit];
+		fillQ ();
+	}
+
+	//Fill the Q from any element
+	private void fillQ (int i = 0)
+	{
+		if( i < qLimit )
+		{
+			newPiece (i);
+			addToQueueList (i);
+			fillQ (++i);
+		}
+	}
+
+	//Push a new piece as the newer element and pops the older
+	private void newPiece (int i)
+	{
+		q[i] = Random.Range (0, groups.Length);
 	}
 
 	//Returns the current piece index in the queue and generate a new one
 	public int next ()
 	{
-		int currentPiece = q;
-		newPiece ();
+		int currentPiece = q[0];
 		flushQueue ();
+		fillQ (qLimit-1);
 		return currentPiece;
 	}
 
-	private void newPiece ()
-	{
-		q = Random.Range (0, groups.Length);
-	}
-
+	//Removes the newer piece in the queue and adds the new one
 	private void flushQueue ()
 	{
-		GameObject[] previousPieces = GameObject.FindGameObjectsWithTag ("Previous");
-		Destroy(previousPieces[0]);
-		addToQueueList ();
+		GameObject[] currentPieces = GameObject.FindGameObjectsWithTag ("Previous");
+		Destroy(currentPieces[0]);
+		rotateQ ();
 	}
 
-	private void addToQueueList ()
+	//Move all the elements in the Q one position up
+	private void rotateQ (int i = 0)
 	{
-		Instantiate (groups[q],
-		             transform.position + new Vector3(0, 0, -1f),
+		if( i < qLimit - 1 )
+		{
+			q[i] = q[++i];
+			GameObject[] previousPieces = GameObject.FindGameObjectsWithTag ("Previous");
+			previousPieces[i].transform.position += new Vector3(0, 5f, 0);
+			rotateQ (i);
+		}
+	}
+
+	//Create and place a new instance of the current piece
+	private void addToQueueList (int i)
+	{
+		Instantiate (groups[q[i]],
+		             transform.position + new Vector3(0, i * -5f, -1f),
 		             Quaternion.identity);
 	}
 	
